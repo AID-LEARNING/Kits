87bca3ae8cf374e3e7408039a6646291926c67f6
Sensei Tarzan <gabrielcaptari0@gmail.com>
2023-04-25 15:33:05 +0200

    apply good format for id

--- src/SenseiTarzan/Kits/Component/KitManager.php
+++ src/SenseiTarzan/Kits/Component/KitManager.php
@@ -9,6 +9,7 @@ use pocketmine\utils\Config;
 use pocketmine\utils\SingletonTrait;
 use SenseiTarzan\Kits\Class\Kits\Kit;
 use SenseiTarzan\Kits\Main;
+use SenseiTarzan\Kits\Utils\Format;
 use SenseiTarzan\LanguageSystem\Component\LanguageManager;
 use SenseiTarzan\Path\PathScanner;
 use SenseiTarzan\Kits\Utils\CustomKnownTranslationFactory;
@@ -59,7 +60,7 @@ class KitManager
      */
     public function getKit(string $name): ?Kit
     {
-        return $this->kits[strtolower($name)] ?? null;
+        return $this->kits[Format::nameToId($name)] ?? null;
     }
 
     public function existKit(string $name): bool
